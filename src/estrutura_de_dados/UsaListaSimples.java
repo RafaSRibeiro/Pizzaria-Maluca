2740874340f0fef68bcd0b27234f133a6f952dd0
rafael.s.ribeiro <rafael.s.ribeiro@sc.senai.br>
2018-10-12 18:31:15 -0300

    configuraçãos de projeto

--- src/estrutura_de_dados/UsaListaSimples.java
+++ src/estrutura_de_dados/UsaListaSimples.java
@@ -1,27 +1,28 @@
 package estrutura_de_dados;
 
 import javax.swing.*;
+
 public class UsaListaSimples {
 
-    public static void main(String args[]){
-    		NoProjeto no = new NoProjeto("t",0);
+    public static void main(String args[]) {
+        NoProjeto no = new NoProjeto("t", 0);
         ListaSimples ls = new ListaSimples();
-        String nome= "inicio";
+        String nome = "inicio";
         int tempo;
-		while (!nome.equals("fim")) {
-	    	nome= JOptionPane.showInputDialog("Nome Etapa: ");
-			if (!nome.equals("fim")) {
-        	tempo= Integer.parseInt(JOptionPane.showInputDialog("Tempo Duracao: "));
-				ls.insereUltimo(nome,tempo);
-			}
-	    }
-    	JOptionPane.showMessageDialog(null,"listando...");
-    	no = ls.primeiro();
-		while (no.proximo != null) {
-			JOptionPane.showMessageDialog(null,"Atual= "+no.nomeEtapa+" - "+no.proximo.nomeEtapa);
-
-			no = no.proximo;
+        while (!nome.equals("fim")) {
+            nome = JOptionPane.showInputDialog("Nome Etapa: ");
+            if (!nome.equals("fim")) {
+                tempo = Integer.parseInt(JOptionPane.showInputDialog("Tempo Duracao: "));
+                ls.insereUltimo(nome, tempo);
+            }
         }
-		JOptionPane.showMessageDialog(null,"Atual= "+no.nomeEtapa+" - fim da lista");
+        JOptionPane.showMessageDialog(null, "listando...");
+        no = ls.primeiro();
+        while (no.proximo != null) {
+            JOptionPane.showMessageDialog(null, "Atual= " + no.nomeEtapa + " - " + no.proximo.nomeEtapa);
+
+            no = no.proximo;
+        }
+        JOptionPane.showMessageDialog(null, "Atual= " + no.nomeEtapa + " - fim da lista");
     }
 }
