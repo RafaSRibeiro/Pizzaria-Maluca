d2665be8ef72a511f83859b081e10b74650eedd9
rafael.s.ribeiro <rafael.s.ribeiro@sc.senai.br>
2018-10-13 11:14:06 -0300

    refatoração de código

--- src/library/Pizza.java
+++ src/library/Pizza.java
@@ -4,11 +4,31 @@ public abstract class Pizza {
 
     protected boolean rodadaSemJogar;
 
+    protected int jogador;
+
+    protected int quantidade;
+
     public boolean isRodadaSemJogar() {
         return rodadaSemJogar;
     }
 
     public void setRodadaSemJogar(boolean rodadaSemJogar) {
         this.rodadaSemJogar = rodadaSemJogar;
+    }
+
+    public int getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(int jogador) {
+        this.jogador = jogador;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 }
